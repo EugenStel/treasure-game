ceca44817b702eea99dc6343db12cec8f94348d3
Web <web@it.step>
2021-08-15 20:41:42 +0300

    implemented game logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,5 @@
-import {getDistanceToTreasure} from './utils/getDistanceToTreasure.js';
-import {generateTreasureCoords} from './utils/generateTreasureCoords.js';
+import {mapClickHandler} from './utils/gameHelpers.js';
 
-const hint = document.getElementById('hint');
-const trasureMap = document.querySelector('[alt="map"]');
+const treasureMap = document.querySelector('[alt="map"]');
 
-console.log(getDistanceToTreasure( {x: 2, y: 10}, {x: 3, y: 8} ));
-console.log(generateTreasureCoords({width:500, height: 500}, 20));
+treasureMap.addEventListener('click', mapClickHandler);
